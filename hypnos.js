5d1be4f3e4f53c7f2f63137dc53e0790750e3a90
sedac0 <122641865+sedac0@users.noreply.github.com>
2023-01-13 22:50:48 +0300

    Add files via upload

---
+++ hypnos.js
@@ -0,0 +1,188 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + "  Token Sese Sokma | Hostlandı");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const client1 = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+const client8 = new Discord.Client();
+const client9 = new Discord.Client();
+const client10 = new Discord.Client();
+const client11 = new Discord.Client();
+const client12 = new Discord.Client();
+const client13 = new Discord.Client();
+const client14 = new Discord.Client();
+const client15 = new Discord.Client();
+const client16 = new Discord.Client();
+const client17 = new Discord.Client();
+const client18 = new Discord.Client();
+const client19 = new Discord.Client();
+const client20 = new Discord.Client();
+const client21 = new Discord.Client();
+const client22 = new Discord.Client();
+const client23 = new Discord.Client();
+const client24 = new Discord.Client();
+const client25 = new Discord.Client();
+
+
+
+const data = new Map();
+
+
+
+client.login("")//Hesap Tokeni Token
+
+client.on('ready', ()=>{
+client.channels.get('').join()//ses kanal id
+})
+
+client1.login("")
+
+client1.on('ready', ()=>{
+client1.channels.get('').join()
+})
+
+
+client2.login("")
+
+
+client2.on('ready', ()=>{
+client2.channels.get('').join()
+})
+
+
+client3.login("")
+
+client3.on('ready', ()=>{
+client3.channels.get('').join()
+})
+
+
+
+client4.login("")
+
+client4.on('ready', ()=>{
+client4.channels.get('').join()
+})
+
+
+client5.login("")
+
+client5.on('ready', ()=>{
+client5.channels.get('').join()
+})
+
+client6.login("")
+
+client6.on('ready', ()=>{
+client6.channels.get('').join()
+})
+
+client7.login("")
+
+client7.on('ready', ()=>{
+client7.channels.get('').join()
+})
+
+client8.login("")
+
+client8.on('ready', ()=>{
+client8.channels.get('').join()
+})
+
+client9.login("")
+
+client9.on('ready', ()=>{
+client9.channels.get('').join()
+})
+
+client10.login("")
+
+client10.on('ready', ()=>{
+client10.channels.get('').join()
+})
+
+client11.login("")
+
+client11.on('ready', ()=>{
+client11.channels.get('').join()
+})
+
+
+client12.login("")
+
+client12.on('ready', ()=>{
+client12.channels.get('').join()
+})
+
+client13.login("")
+
+client13.on('ready', ()=>{
+client13.channels.get('').join()
+})
+
+client14.login("")
+
+client14.on('ready', ()=>{
+client14.channels.get('').join()
+})
+
+client16.login("")
+
+client16.on('ready', ()=>{
+client16.channels.get('').join()
+})
+
+client17.login("")
+
+client17.on('ready', ()=>{
+client17.channels.get('').join()
+})
+
+
+client18.login("")
+
+client18.on('ready', ()=>{
+client18.channels.get('').join()
+})
+
+
+
+client19.login("")
+
+client19.on('ready', ()=>{
+client19.channels.get('').join()
+})
+
+client20.login("")
+
+client20.on('ready', ()=>{
+client20.channels.get('').join()
+})
+
+client21.login("")
+client21.on('ready', ()=>{
+client21.channels.get('').join()
+})
